06bbf6f5b74d2f04dde44908767fbac7423a5513
BuildTools <unconfigured@null.spigotmc.org>
2022-07-30 02:13:53 +0800

    updates

--- src/main/java/net/zylll/fabric_mod/tool/weapon/PoopSword.java
+++ src/main/java/net/zylll/fabric_mod/tool/weapon/PoopSword.java
@@ -1,10 +1,40 @@
 package net.zylll.fabric_mod.tool.weapon;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+import net.zylll.fabric_mod.item.AllItems;
+
+import java.util.Random;
 
 public class PoopSword extends SwordItem {
     public PoopSword(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
         super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public boolean canMine(BlockState state, World world, BlockPos pos, PlayerEntity miner) {
+        return true;//创造执剑可破坏方块
+    }
+
+    @Override
+    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+        Random random = new Random();
+        if(random.nextFloat() - 0.9F > 0.0F){
+        ((PlayerEntity)miner).giveItemStack(AllItems.POOP.getDefaultStack());}
+        return super.postMine(stack, world, state, pos, miner);//10%获得poop
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        World world = attacker.getWorld();
+        world.createExplosion(attacker, attacker.getX(), attacker.getY(), attacker.getZ(), 0.5F, false, Explosion.DestructionType.NONE);
+        return super.postHit(stack, target, attacker);//攻击爆炸
     }
 }
