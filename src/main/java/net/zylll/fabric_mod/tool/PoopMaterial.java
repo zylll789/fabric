698699dea79f97fdedf021d38c77c94489a8ec8d
BuildTools <unconfigured@null.spigotmc.org>
2022-07-30 01:21:17 +0800

    1

--- src/main/java/net/zylll/fabric_mod/tool/PoopMaterial.java
+++ src/main/java/net/zylll/fabric_mod/tool/PoopMaterial.java
@@ -1,4 +1,37 @@
 package net.zylll.fabric_mod.tool;
 
-public class PoopMaterial {
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.zylll.fabric_mod.item.AllItems;
+
+public class PoopMaterial implements ToolMaterial {
+    @Override
+    public int getDurability() {
+        return 10;//耐久
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 0;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return -1;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 9;//附魔
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(AllItems.POOP);//修复物品
+    }
 }
---
+++ src/main/java/net/zylll/fabric_mod/tool/weapon/PoopSword.java
@@ -0,0 +1,10 @@
+package net.zylll.fabric_mod.tool.weapon;
+
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class PoopSword extends SwordItem {
+    public PoopSword(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+}
