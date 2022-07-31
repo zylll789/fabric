7af72aeead96dbe5d477c74e478af681468fbef0
BuildTools <unconfigured@null.spigotmc.org>
2022-07-31 18:27:13 +0800

    updates

--- src/main/java/net/zylll/fabric_mod/block/AllBlocks.java
+++ src/main/java/net/zylll/fabric_mod/block/AllBlocks.java
@@ -1,13 +1,16 @@
 package net.zylll.fabric_mod.block;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
 
 public class AllBlocks {
     //all blocks are in this
     //
-    public static final PoopBlock POOP_BLOCK = new PoopBlock(FabricBlockSettings.of(Material.SNOW_BLOCK).strength(0.2F,0.1F).sounds(BlockSoundGroup.SNOW));
+    public static final PoopBlock POOP_BLOCK = new PoopBlock(FabricBlockSettings.of(Material.SNOW_BLOCK).strength
+            (0.2F,0.1F).sounds(BlockSoundGroup.SNOW).jumpVelocityMultiplier
+            (0.8F).velocityMultiplier(0.8F).mapColor(MapColor.YELLOW).allowsSpawning(PoopBlock::canSpawn));
 
 
 }
--- src/main/java/net/zylll/fabric_mod/block/PoopBlock.java
+++ src/main/java/net/zylll/fabric_mod/block/PoopBlock.java
@@ -1,6 +1,10 @@
 package net.zylll.fabric_mod.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 
 public class PoopBlock extends Block{
 
@@ -8,4 +12,7 @@ public class PoopBlock extends Block{
         super(settings);
     }
 
+    public static boolean canSpawn(BlockState blockState, BlockView blockView, BlockPos pos, EntityType<?> entityType) {
+        return false;
+    }
 }
