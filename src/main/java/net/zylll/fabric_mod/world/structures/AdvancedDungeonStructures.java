710efb6210d1fc5e70010843d78e9e7b3ea131f2
BuildTools <unconfigured@null.spigotmc.org>
2023-01-31 17:57:57 +0800

    structures

---
+++ src/main/java/net/zylll/fabric_mod/world/structures/AdvancedDungeonStructures.java
@@ -0,0 +1,78 @@
+package net.zylll.fabric_mod.world.structures;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.structure.PoolStructurePiece;
+import net.minecraft.structure.StructureGeneratorFactory;
+import net.minecraft.structure.StructurePiecesGenerator;
+import net.minecraft.structure.StructureSetKeys;
+import net.minecraft.structure.pool.StructurePool;
+import net.minecraft.structure.pool.StructurePoolBasedGenerator;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.gen.feature.StructureFeature;
+import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
+import net.zylll.fabric_mod.FabricMod;
+import org.apache.logging.log4j.Level;
+
+import java.util.Optional;
+
+public class AdvancedDungeonStructures extends StructureFeature<StructurePoolFeatureConfig> {
+    public AdvancedDungeonStructures() {
+        super(CODEC, AdvancedDungeonStructures::createPiecesGenerator);
+    }
+
+    public static final Codec<StructurePoolFeatureConfig> CODEC = RecordCodecBuilder.create(
+            instance -> instance.group(
+                    StructurePool.REGISTRY_CODEC.fieldOf("start_pool").forGetter(StructurePoolFeatureConfig::getStartPool),
+                    Codec.intRange(0, 64).fieldOf("size").forGetter(StructurePoolFeatureConfig::getSize))
+                    .apply(instance, StructurePoolFeatureConfig::new)
+    );
+
+    public static Optional<StructurePiecesGenerator<StructurePoolFeatureConfig>> createPiecesGenerator(StructureGeneratorFactory.Context<StructurePoolFeatureConfig> context) {
+
+        // Check if the spot is valid for our structure. This is just as another method for cleanness.
+        // Returning an empty optional tells the game to skip this spot as it will not generate the structure.
+        if (!isFeatureChunk(context)) {
+            return Optional.empty();
+        }
+
+        // Turns the chunk coordinates into actual coordinates we can use. (Gets center of that chunk)
+        BlockPos blockpos = context.chunkPos().getCenterAtY(0);
+
+        Optional<StructurePiecesGenerator<StructurePoolFeatureConfig>> structurePiecesGenerator =
+                StructurePoolBasedGenerator.generate(
+                        context, // Used for JigsawPlacement to get all the proper behaviors done.
+                        PoolStructurePiece::new, // Needed in order to create a list of jigsaw pieces when making the structure's layout.
+                        blockpos, // Position of the structure. Y value is ignored if last parameter is set to true.
+                        false,  // Special boundary adjustments for villages. It's... hard to explain. Keep this false and make your pieces not be partially intersecting.
+                        // Either not intersecting or fully contained will make children pieces spawn just fine. It's easier that way.
+                        true // Adds the terrain height's y value to the passed in blockpos's y value. (This uses WORLD_SURFACE_WG heightmap which stops at top water too)
+                        // Here, blockpos's y value is 60 which means the structure spawn 60 blocks above terrain height.
+                        // Set this to false for structure to be place only at the passed in blockpos's Y value instead.
+                        // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
+                );
+
+        /*
+         * Note, you are always free to make your own JigsawPlacement class and implementation of how the structure
+         * should generate. It is tricky but extremely powerful if you are doing something that vanilla's jigsaw system cannot do.
+         * Such as for example, forcing 3 pieces to always spawn every time, limiting how often a piece spawns, or remove the intersection limitation of pieces.
+         *
+         * An example of a custom JigsawPlacement.addPieces in action can be found here (warning, it is using Mojmap mappings):
+         * https://github.com/TelepathicGrunt/RepurposedStructures/blob/1.18.2/src/main/java/com/telepathicgrunt/repurposedstructures/world/structures/pieces/PieceLimitedJigsawManager.java
+         */
+
+        if (structurePiecesGenerator.isPresent()) {
+            FabricMod.LOGGER.log(Level.DEBUG, "Advanced Dungeon at {}", blockpos);
+        }
+        return structurePiecesGenerator;
+    }
+
+    private static boolean isFeatureChunk(StructureGeneratorFactory.Context<StructurePoolFeatureConfig> context) {
+        ChunkPos chunkpos = context.chunkPos();
+
+        // Checks to make sure our structure does not spawn within 10 chunks of an Ocean Monument
+        // to demonstrate how this method is good for checking extra conditions for spawning
+        return !context.chunkGenerator().method_41053(StructureSetKeys.OCEAN_MONUMENTS, context.seed(), chunkpos.x, chunkpos.z, 10);
+    }
+}
